d2e076292da2ca1652851bd491249403a9aa7df0
pes <psaarini@gmail.com>
2019-07-13 18:05:43 +0300

    updated dependencies and added message if blog posts are empty

--- app.js
+++ app.js
@@ -15,82 +15,87 @@ app.use("/", express.static(__dirname + "/public/"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.render("home");
+  res.render("home");
 });
 
 app.get("/blog", (req, res) => {
-	fs.readFile("./blog-posts.txt", "utf-8", (err, file) => {
-		if (err) console.log(err);
-		else {
-			blogPosts = JSON.parse(file);
-		}
+  fs.readFile("./blog-posts.txt", "utf-8", (err, file) => {
+    if (err) console.log(err);
+    else {
+      blogPosts = JSON.parse(file);
+    }
 
-		res.render("blog", {
-			posts: blogPosts
-		});
-	});
+    res.render("blog", {
+      posts: blogPosts
+    });
+  });
 });
 
-
-app.get("/compose", function (req, res) {
-	res.render("compose");
+app.get("/compose", function(req, res) {
+  res.render("compose");
 });
 
-app.post("/compose", function (req, res) {
-	const post = {
-		title: req.body.blogTitle,
-		content: req.body.blogBody,
-		comments: []
-	};
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.blogTitle,
+    content: req.body.blogBody,
+    comments: []
+  };
 
-	blogPosts.push(post);
+  blogPosts.push(post);
 
-	saveBlogPosts();
+  saveBlogPosts();
 
-	res.redirect("/blog");
+  res.redirect("/blog");
 });
 
-app.get("/posts/:postName", function (req, res) {
-	const requestedTitle = req.params.postName;
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = req.params.postName;
 
-	blogPosts.forEach(function (post) {
-		if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
-			res.render("post", {
-				title: post.title,
-				content: post.content,
-				comments: post.comments
-			});
-		}
-	});
-
+  blogPosts.forEach(function(post) {
+    if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+        comments: post.comments
+      });
+    }
+  });
 });
 
+app.post("/posts/:postName", function(req, res) {
+  const requestedTitle = req.params.postName;
+  const comment = req.body.comment;
 
-app.post("/posts/:postName", function (req, res) {
-	const requestedTitle = req.params.postName;
-	const comment = req.body.comment;
+  blogPosts.forEach(function(post) {
+    if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
+      post.comments.push(comment);
+    }
+  });
 
-	blogPosts.forEach(function (post) {
-		if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
-			post.comments.push(comment);
-		};
-	});
-
-	saveBlogPosts();
-	res.redirect("/posts/" + requestedTitle);
+  saveBlogPosts();
+  res.redirect("/posts/" + requestedTitle);
 });
-
 
 app.listen(PORT, () => {
-	console.log("Listening to port " + PORT);
+  console.log("Listening to port " + PORT);
 });
 
 function saveBlogPosts() {
-	let jsonData = JSON.stringify(blogPosts);
+  let jsonData = JSON.stringify(blogPosts);
 
-	fs.writeFile("blog-posts.txt", jsonData, function (err) {
-		if (err) {
-			console.log(err);
-		}
-	});
+  fs.writeFile("blog-posts.txt", jsonData, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
+
+// function htmlEscape(str) {
+// 	return String(str)
+// 		.replace(/&/g, '&amp;')
+// 		.replace(/"/g, '&quot;')
+// 		.replace(/'/g, '&#39;')
+// 		.replace(/</g, '&lt;')
+// 		.replace(/>/g, '&gt;');
+// }
